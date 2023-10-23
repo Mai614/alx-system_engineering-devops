89fdf83eb90ae2caedd1f99c27d52f77ed57b89d
Mai614 <maymohamed614@gmail.com>
2023-10-23 12:51:57 +0000

    executed

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,6 +9,15 @@ import requests
 import sys
 
 def get_employee_todo_progress(employee_id):
+    """
+    Fetches and displays the TODO list progress for a given employee ID from the JSONPlaceholder API.
+
+    Args:
+        employee_id (int): The ID of the employee whose TODO list progress is to be fetched.
+
+    Returns:
+        None: Prints the employee's TODO list progress to the standard output.
+    """
     base_url = f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
     response = requests.get(base_url)
 
@@ -25,7 +34,7 @@ def get_employee_todo_progress(employee_id):
     user_data = user_response.json()
     employee_name = user_data.get('name', 'Unknown Employee')
 
-    print(f"Employee {employee_name} is done with tasks({num_completed_tasks}/{total_tasks}):")
+    print(f"Employee {employee_name} is done with tasks ({num_completed_tasks}/{total_tasks}):")
     for task in completed_tasks:
         print(f"\t{task}")
 

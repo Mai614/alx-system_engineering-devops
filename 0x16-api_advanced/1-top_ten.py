5a69290e356dac1e1e706216aaa85455608571af
root <maymohamed614@gmail.com>
2024-01-16 19:50:11 +0000

    top-ten

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+"""
+Prints the titles of the first 10 hot posts listed for a given subreddit.
+"""
+
+import requests
+
+def top_ten(subreddit):
+    """
+    Function that queries the Reddit API and prints the titles of the first
+    10 hot posts listed for a given subreddit.
+    """
+
+    if subreddit is None or not isinstance(subreddit, str):
+        print("None")
+        return
+
+    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    params = {'limit': 10}
+    url = f'https://www.reddit.com/r/{subreddit}/hot/.json'
+
+    response = requests.get(url, headers=user_agent, params=params)
+    results = response.json()
+
+    try:
+        my_data = results.get('data').get('children')
+
+        for i in my_data:
+            print(i.get('data').get('title'))
+
+    except Exception:
+        print("None")
+
+if __name__ == "__main__":
+    pass
+

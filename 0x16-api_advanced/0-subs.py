b3c3172edadd56526e39f7d2ae0ea8c13bb451fb
root <maymohamed614@gmail.com>
2024-01-16 19:08:06 +0000

    subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+import requests
+
 def number_of_subscribers(subreddit):
     """
     Queries the Reddit API and returns the number of subscribers to the subreddit.
@@ -10,11 +12,11 @@ def number_of_subscribers(subreddit):
     Returns:
         int: The number of subscribers to the subreddit. Returns 0 if the subreddit is not found or an error occurs.
     """
-    import requests
-
-    sub_info = requests.get("https://www.reddit.com/r/{}/about.json".format(subreddit),
-                            headers={"User-Agent": "My-User-Agent"},
-                            allow_redirects=False)
+    sub_info = requests.get(
+        "https://www.reddit.com/r/{}/about.json".format(subreddit),
+        headers={"User-Agent": "My-User-Agent"},
+        allow_redirects=False
+    )
 
     if sub_info.status_code >= 300:
         return 0
@@ -22,3 +24,4 @@ def number_of_subscribers(subreddit):
     subscribers_count = sub_info.json().get("data", {}).get("subscribers", 0)
 
     return subscribers_count
+
